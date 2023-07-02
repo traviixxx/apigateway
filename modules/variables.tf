39a525a4fe71d1ec7076362c83c51c78e764949c
traviixxx <92700220+traviixxx@users.noreply.github.com>
2023-07-02 13:36:36 +0700

    Delete modules directory

--- modules/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  type        = string
-  description = "The region in which to create/manage resources"
-  default     = "ap-northeast-1"
-}
-variable "account_id"{
-  type        = string
-  description = "The account ID in which to create/manage resources"
-}
